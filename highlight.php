776909f7ee1e56021bd3ab0f6992ae661717602b
Bast22 <patriciaheimfarth@gmail.com>
2023-04-02 11:29:02 +0200

    comment

--- highlight.php
+++ highlight.php
@@ -1,17 +1,17 @@
 <?php
 /*
- * Plugin Name:       Highlight
- * Plugin URI:        https://example.com 
+ * Plugin Name:       Highlight Background
+ * Plugin URI:        https://heimpa.de/
  * Description:       Highlight
  * Version:           0.0.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Patricia Heimfarth
- * Author URI:        https://author.example.com/
+ * Author URI:        https://heimpa.de/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
+ * Update URI:        https://example.com/hightlight-background/
+ * Text Domain:       highlight
  * Domain Path:       /languages
  */
 
